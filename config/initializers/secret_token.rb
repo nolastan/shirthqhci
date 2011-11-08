eaa4612d6b6c8172ccd31ed837d0b0316f0094c6
Stanford Rosenthal <nolastan@gmail.com>
2011-11-08 16:47:50 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shirthqhci::Application.config.secret_token = 'fb7e40da161f6ca570a6851369ff0a86707c37b25209a2877f7a03b2b0ce6ddd0fa37a260894e8302a9d84ce89db8db568ce31ab183398acb50b247ae2d91ec5'
